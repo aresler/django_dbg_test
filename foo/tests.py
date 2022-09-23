e1e5011fcc1739f01807958f62b68531116e0457
andrew <andrey.resler@yandex.ru>
2022-09-23 14:35:18 +0200

    Added test mocks

--- foo/tests.py
+++ foo/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestStringMethods(TestCase):
+
+    def setUp(self):
+        print('Setup mock...')
+
+    def test_1(self):
+        print('Assert mock 1...')
+        self.assertTrue(True)
+
+    def test_2(self):
+        print('Assert mock 2...')
+        self.assertTrue(True)
+
+    def tearDown(self):
+        print('Tear down mock...')
